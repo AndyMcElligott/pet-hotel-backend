bae9b95202f591f8cff0d8c1e20c07069512ab5b
Gabriel Hawk <crimsonhawk47@gmail.com>
2020-02-27 11:53:13 -0600

    Added owners route

--- app.py
+++ app.py
@@ -8,10 +8,24 @@ conn = psycopg2.connect(database="pet-hotel", host = "localhost", port = "5432")
 
 
 @app.route('/api/history',  methods=['GET', 'POST', 'PUT'])
-def hello_world():
+def getHistory():
 
   cur = conn.cursor()
   cur.execute('SELECT * FROM "history"')
+  rows = cur.fetchall()
+
+  print(rows)
+  print([desc[0] for desc in cur.description])
+  # print(cur.description)
+  cur.close()
+  return jsonify(rows)
+
+
+@app.route('/api/owners',  methods=['GET', 'POST', 'PUT'])
+def getOwners():
+
+  cur = conn.cursor()
+  cur.execute('SELECT * FROM "owners"')
   rows = cur.fetchall()
 
   print(rows)
