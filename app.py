0741b705ef140d85ed505a36c0a05e31061f09dd
Gabriel Hawk <crimsonhawk47@gmail.com>
2020-02-27 16:45:48 -0600

    Returning routes as object

--- app.py
+++ app.py
@@ -1,10 +1,32 @@
 from flask import Flask
 from flask import request
 from flask import jsonify
+from collections import defaultdict
 import psycopg2
 app = Flask(__name__)
 
 conn = psycopg2.connect(database="pet-hotel", host = "localhost", port = "5432")
+
+from collections import namedtuple
+
+# def create_record(obj, fields):
+#     ''' given obj from db returns named tuple with fields mapped to values '''
+#     Record = namedtuple("Record", fields)
+#     mappings = dict(zip(fields, obj))
+#     return Record(**mappings)
+
+def make_dictionary(row, columns):
+  newDict = {}
+  for index in range(len(row)):
+    # print(columns[index], ' ', row[index])
+    key = columns[index]
+    value = row[index]
+    newDict[key] = value
+  return newDict
+    
+
+
+
 
 
 @app.route('/api/history',  methods=['GET', 'POST', 'PUT'])
@@ -12,26 +34,31 @@ def getHistory():
 
   cur = conn.cursor()
   cur.execute('SELECT * FROM "history"')
+
   rows = cur.fetchall()
+  columns = [desc[0] for desc in cur.description]
+  arrayOfHistory = []
+  for row in rows:
+    arrayOfHistory.append(make_dictionary(row, columns))
 
-  print(rows)
-  print([desc[0] for desc in cur.description])
-  # print(cur.description)
   cur.close()
-  return jsonify(rows)
+  
+  return jsonify(arrayOfHistory)
 
 
-@app.route('/api/owners',  methods=['GET', 'POST', 'PUT'])
+@app.route('/api/owners',  methods=['GET', 'POST', 'PUT', 'DELETE'])
 def getOwners():
 
   cur = conn.cursor()
   cur.execute('SELECT * FROM "owners"')
-  rows = cur.fetchall()
 
-  print(rows)
-  print([desc[0] for desc in cur.description])
-  # print(cur.description)
+  rows = cur.fetchall()
+  columns = [desc[0] for desc in cur.description]
+  arrayOfOwners = []
+  for row in rows:
+    arrayOfOwners.append(make_dictionary(row, columns))
+
   cur.close()
-  return jsonify(rows)
+  return jsonify(arrayOfOwners)
 
 
