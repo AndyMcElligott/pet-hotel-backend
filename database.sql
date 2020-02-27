f65d32de906e18b28676c4a2c2a73c8b8a4c9554
luke rohde <morneau33@gmail.com>
2020-02-27 11:25:10 -0600

    modified database.sql code

--- database.sql
+++ database.sql
@@ -1,22 +1,26 @@
-drop table "history";
+CREATE TABLE "owners" (
+	"id" SERIAL PRIMARY KEY,
+	"name" varchar(255) NOT NULL);
+
+INSERT INTO "owners" ("name")
+VALUES('Gabriel'), ('Luke'), ('Meghan');
+
+DROP TABLE "owners";
 
 CREATE TABLE "history" (
-	"id" serial NOT NULL,
-	"owner" varchar(255) NOT NULL,
+	"id" SERIAL PRIMARY KEY,
+	"owner" INT NOT NULL REFERENCES "owners",
 	"pet" varchar(255) NOT NULL,
 	"breed" varchar(255) NOT NULL,
 	"color" varchar(255) NOT NULL,
-	"checked-in" varchar(255) NOT NULL,
-	"actions" varchar(255) NOT NULL); 
-	
-drop table "owners";
+	"checked-in" boolean NOT NULL);
 
-create table "owners" (
-	"id" serial NOT NULL,
-	"name" varchar(255) NOT NULL,
-	"number_of_pets" varchar(255) NOT NULL,
-	"actions" varchar(255) NOT NULL);
+INSERT INTO "history" ("owner", "pet", "breed", "color", "checked-in")
+VALUES('1', 'Tina', 'supertuna', 'yellow', 'true'),
+('3', 'Alfie', 'Pibble', 'Fawn', 'true'),
+('2', 'Scout', 'Doggo', 'Tuxedo', 'false'),
+('1', 'Carl', 'Cat', 'Calico', 'true'),
+('1', 'Padre', 'Cat', 'White', 'false'),
+('1', 'Teeny', 'Cat', 'Big', 'false');
 
-INSERT INTO "history" ("owner", "pet", "breed", "color", "checked-in", "actions")
-
-values('Andy', 'Tuna', 'supertuna', 'yellow', 'yes', 'tunajump');
+DROP TABLE "history";
