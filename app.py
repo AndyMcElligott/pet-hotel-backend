95d08a119c5bded29873c1bb2277f0c51a2252ca
Gabriel Hawk <crimsonhawk47@gmail.com>
2020-02-28 09:23:33 -0600

    Refactored server

--- app.py
+++ app.py
@@ -5,60 +5,46 @@ from collections import defaultdict
 import psycopg2
 app = Flask(__name__)
 
-conn = psycopg2.connect(database="pet-hotel", host = "localhost", port = "5432")
+conn = psycopg2.connect(database="pet-hotel", host="localhost", port="5432")
 
-from collections import namedtuple
-
-# def create_record(obj, fields):
-#     ''' given obj from db returns named tuple with fields mapped to values '''
-#     Record = namedtuple("Record", fields)
-#     mappings = dict(zip(fields, obj))
-#     return Record(**mappings)
-
-def make_dictionary(row, columns):
-  newDict = {}
-  for index in range(len(row)):
-    # print(columns[index], ' ', row[index])
-    key = columns[index]
-    value = row[index]
-    newDict[key] = value
-  return newDict
-    
+# def make_dictionary(row, columns):
+#   newDict = {}
+#   for index in range(len(row)):
+#     # print(columns[index], ' ', row[index])
+#     key = columns[index]
+#     value = row[index]
+#     newDict[key] = value
+#   return newDict
 
 
+def make_dictionary(columns, row):
+    rowWithKeys = dict(zip(columns, row))
+    return rowWithKeys
 
+
+def sqlSelect(query):
+    cur = conn.cursor()
+    cur.execute(query)
+
+    rows = cur.fetchall()
+    columns = [desc[0] for desc in cur.description]
+    rowsWithKeys = []
+    for row in rows:
+        rowsWithKeys.append(make_dictionary(columns, row))
+
+    cur.close()
+    return rowsWithKeys
 
 
 @app.route('/api/history',  methods=['GET', 'POST', 'PUT'])
 def getHistory():
 
-  cur = conn.cursor()
-  cur.execute('SELECT * FROM "history"')
-
-  rows = cur.fetchall()
-  columns = [desc[0] for desc in cur.description]
-  arrayOfHistory = []
-  for row in rows:
-    arrayOfHistory.append(make_dictionary(row, columns))
-
-  cur.close()
-  
-  return jsonify(arrayOfHistory)
+    arrayOfHistory = sqlSelect('SELECT * FROM "history"')
+    return jsonify(arrayOfHistory)
 
 
 @app.route('/api/owners',  methods=['GET', 'POST', 'PUT', 'DELETE'])
 def getOwners():
 
-  cur = conn.cursor()
-  cur.execute('SELECT * FROM "owners"')
-
-  rows = cur.fetchall()
-  columns = [desc[0] for desc in cur.description]
-  arrayOfOwners = []
-  for row in rows:
-    arrayOfOwners.append(make_dictionary(row, columns))
-
-  cur.close()
-  return jsonify(arrayOfOwners)
-
-
+    arrayOfOwners = sqlSelect('SELECT * FROM "owners"')
+    return jsonify(arrayOfOwners)
